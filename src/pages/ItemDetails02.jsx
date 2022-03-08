1939c8c444f21f7d43643a85baad3875a5d99c3b
Ken Erwin <ken@devopslibrary.com>
2022-03-08 13:23:42 -0500

    added follow twitter and join discord link

--- src/pages/ItemDetails02.jsx
+++ src/pages/ItemDetails02.jsx
@@ -16,6 +16,7 @@ import {useFetch} from "../lib/useFetch";
 import {Timeline} from "react-twitter-widgets";
 import {Accordion} from "react-bootstrap-accordion";
 import {FlagsComponent} from "../components/flagsComponent";
+import {TwitterFollowButton} from "react-twitter-embed";
 
 const ItemDetails02 = () => {
     const {id} = useParams();
@@ -104,6 +105,7 @@ const ItemDetails02 = () => {
                                         }
                                     </div>
                                 </div>
+
                                 {nftData.faq && (
                                 <div className="pad-t-17 pad-b-54"><h2>FAQ</h2>
                                     {
@@ -113,6 +115,7 @@ const ItemDetails02 = () => {
                                           </Accordion>
                                         ))
                                     }
+
                                 </div>
                                 )}
                             </div>
@@ -123,14 +126,9 @@ const ItemDetails02 = () => {
                                             <div className="left">
                                                 <h2>{nftData.title} Information</h2>
                                             </div>
-                                            <div className="right">
-
-                                                <span className="viewed eye mg-r-8">225</span>
-                                                <span to="/login" className="liked heart wishlist-button"><span
-                                                    className="number-like">100</span></span>
-                                            </div>
                                         </div>
                                         <div className="client-infor sc-card-product">
+
                                             <div className="meta-info">
                                                 <div className="author">
                                                     <div className="avatar">
@@ -152,12 +150,34 @@ const ItemDetails02 = () => {
                                                         <h6><Link to="/author-02">{new Date(nftData.creationTimestamp.replace(" +UTC", "")).toLocaleString()}</Link></h6>
                                                     </div>
                                                 </div>
+
                                             </div>
+
                                         </div>
+                                        <TwitterFollowButton
+                                            screenName={nftData.twitter.replace("@","")}
+                                        />
+
+                                        <div id="discord-button">
+                                            <a href={nftData.discord} >
+                                                <div className="icon">
+                                                    <svg id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 245 240">
+                                                        <path className="st0" d="M104.4 103.9c-5.7 0-10.2 5-10.2 11.1s4.6 11.1 10.2 11.1c5.7 0 10.2-5 10.2-11.1.1-6.1-4.5-11.1-10.2-11.1zM140.9 103.9c-5.7 0-10.2 5-10.2 11.1s4.6 11.1 10.2 11.1c5.7 0 10.2-5 10.2-11.1s-4.5-11.1-10.2-11.1z" />
+                                                        <path className="st0" d="M189.5 20h-134C44.2 20 35 29.2 35 40.6v135.2c0 11.4 9.2 20.6 20.5 20.6h113.4l-5.3-18.5 12.8 11.9 12.1 11.2 21.5 19V40.6c0-11.4-9.2-20.6-20.5-20.6zm-38.6 130.6s-3.6-4.3-6.6-8.1c13.1-3.7 18.1-11.9 18.1-11.9-4.1 2.7-8 4.6-11.5 5.9-5 2.1-9.8 3.5-14.5 4.3-9.6 1.8-18.4 1.3-25.9-.1-5.7-1.1-10.6-2.7-14.7-4.3-2.3-.9-4.8-2-7.3-3.4-.3-.2-.6-.3-.9-.5-.2-.1-.3-.2-.4-.3-1.8-1-2.8-1.7-2.8-1.7s4.8 8 17.5 11.8c-3 3.8-6.7 8.3-6.7 8.3-22.1-.7-30.5-15.2-30.5-15.2 0-32.2 14.4-58.3 14.4-58.3 14.4-10.8 28.1-10.5 28.1-10.5l1 1.2c-18 5.2-26.3 13.1-26.3 13.1s2.2-1.2 5.9-2.9c10.7-4.7 19.2-6 22.7-6.3.6-.1 1.1-.2 1.7-.2 6.1-.8 13-1 20.2-.2 9.5 1.1 19.7 3.9 30.1 9.6 0 0-7.9-7.5-24.9-12.7l1.4-1.6s13.7-.3 28.1 10.5c0 0 14.4 26.1 14.4 58.3 0 0-8.5 14.5-30.6 15.2z" />
+                                                    </svg>
+                                                </div>
+                                                <span><b>Join the Discord</b></span>
+                                            </a>
+                                        </div>
+                                        <br/>
+
                                         <p>{nftData.excerpt}</p>
+
                                         <div className="meta-item-details">
                                             <div className="item-style-2 item-details">
                                                 <ul className="list-details">
+
+                                                    <li><span>Website: </span><h6><a href={nftData.website}>{nftData.website.replace("https://", "").replace("http://", "")}</a></h6></li>
                                                     <li><span>Supply: </span><h6>{nftData.totalSupply}</h6></li>
                                                     <li><span>Standard: </span><h6>{nftData.standard}</h6></li>
                                                     <li><span>Active Development:</span><h6>{nftData.activeDevelopment ? "Yes" : "No"}</h6></li>
